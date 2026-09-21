cb42ac8f9b967c1f44ed6812470a6c8a1544a544
Daiki <330136907+DaikiAono@users.noreply.github.com>
2026-09-21 15:15:52 -0400

    Day 1 upload
    Band Name Generator: A program that produces unique names through string concatenation.

---
+++ Day1/main.py
@@ -0,0 +1,13 @@
+def main():
+    print("Welcome to the band name generator app")
+
+    city_name = input("What is the name of the city that you were born in? ")
+    pet_name = input("What is the name of your pet or a pet you've had before?  ")
+
+    name_generated = city_name + " " + pet_name
+
+
+    print("The name of your band is called: " + name_generated)
+
+
+main()
